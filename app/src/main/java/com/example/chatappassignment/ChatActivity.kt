ca0f8bb7f627c291dcac02736c261478acb1d82e
Azadshekhar <shekharmaurya8978@gmail.com>
2024-01-03 17:37:46 +0530

    adding splash screen

--- app/src/main/java/com/example/chatappassignment/ChatActivity.kt
+++ app/src/main/java/com/example/chatappassignment/ChatActivity.kt
@@ -3,6 +3,8 @@ package com.example.chatappassignment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,6 +18,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.getValue
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
     private lateinit var chatRecyclarview: RecyclerView
     private lateinit var messageBox: EditText
     private lateinit var sendButton: ImageView
@@ -74,5 +77,23 @@ class ChatActivity : AppCompatActivity() {
             messageBox.setText("")
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout) {
+// write the logic for logout
+            mAuth.signOut()
+            val  intent=Intent(this@ChatActivity,LogIn::class.java)
+            finish()
+            startActivity(intent)
+            return true
+        }
+        return true
+        //return super.onOptionsItemSelected(item)
     }
 }
